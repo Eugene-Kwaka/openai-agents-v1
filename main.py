a2e4e2288a3d060264d7b08116c4d0e5576a647a
Eugene-Kwaka <kwakaeugene@gmail.com>
2026-04-17 22:19:45 -0500

    Updated the api key to be stored in a dotenv file

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+from dotenv import load_dotenv
 from openai import OpenAI
+
+
+load_dotenv()
+
+
 client = OpenAI()
 
 response = client.responses.create(
     model="gpt-5.2",
-    input="Write a one-sentence story about AI."
+    input=[
+        {"role": "system", "content": "You are a an experienced tutor who explains complex topics in simple terms with analogies."},
+        {"role": "user", "content": "What is dhcp in networking?"}
+    ]
 )
 
 print(response.output_text)
